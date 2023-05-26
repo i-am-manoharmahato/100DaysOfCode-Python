af059e7912231f63b3f7f96be91199764eba703e
Manohar Kumar <manohar.mahato@outlook.com>
2023-05-27 00:34:03 +1000

    Commiting Day 13 14 practice code

--- Day13-14-20230526/days_in_month.py
+++ Day13-14-20230526/days_in_month.py
@@ -5,8 +5,15 @@ def is_leap_year(year):
         return True
     else:
         return False
-    
+
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 year = int(input("Enter the year: "))
 month = int(input("Enter the month: "))
 
-print (is_leap_year(2012))
+if ((is_leap_year(year) == True) and (month == 2)):
+    resulting_days = 29
+else:
+    resulting_days = days_in_month[month-1]
+
+print(f'Number of days: {resulting_days}')
