bb2a02c3d7b39d4613c3c2fe872704812cb8a83a
Manohar Kumar <manohar.mahato@outlook.com>
2023-06-23 18:08:36 +1000

    turtle draw shape completed

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import *
+import random
+
+timmy = Turtle()
+timmy.shape("arrow")
+timmy.shapesize(1, 1, 3)
+timmy.color("green")
+
+# print(timmy)
+timmy.pensize(5)
+timmy.speed(2)
+
+turtle_colors = ['forest green', 'dark goldenrod', 'indian red', 'teal', 'dark salmon']
+
+
+def draw_shape(number_of_sides):
+    angle = int(360 / number_of_sides)
+    for _ in range(number_of_sides):
+        timmy.fd(100)
+        timmy.right(angle)
+
+
+for sides in range(3, 10):
+    timmy.color(random.choice(turtle_colors))
+    draw_shape(sides)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
